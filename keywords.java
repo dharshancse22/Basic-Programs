2eb79d0cb45ae7c5ef1ffb4a2a42809e5e96c2cf
Dharshanraj N <dharshanr479@gmail.com>
2025-02-04 22:15:50 +0530

    KeyWords and Expression
    keywords.java

---
+++ keywords.java
@@ -0,0 +1,15 @@
+public class keywords {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 20;
+        double c = 30;
+        double sum = a+b*c;
+        System.out.println(sum);
+        float area = 100 * 25;
+        if (area == 2500){
+            
+            System.out.println("float is a area");
+        }
+    }
+    
+}
