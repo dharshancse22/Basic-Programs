cc4396149f6a221e05b0801c8ce4de0d5e26c58f
Dharshanraj N <dharshanr479@gmail.com>
2025-02-11 21:46:54 +0530

    switch.java
    Switch statement is used to test he equality against multiple variable

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[]args){
+        int Myvalue = 6;
+        switch(Myvalue){
+        case 1:
+        System.out.println("Myvalue is 1");
+        break;
+        case 2:
+        System.out.println("Myvalue is 2");
+        break;
+        case 3:
+        System.out.println("Myvalue is 3");
+        break;
+        default:
+        System.out.println("Myvalue is More than 3");
+        break;
+
+        }
+        
+        
+    }
+}
