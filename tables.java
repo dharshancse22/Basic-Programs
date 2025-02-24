19c46b20bc25f12ed7a3e432f577776d26848aed
Dharshanraj N <dharshanr479@gmail.com>
2025-02-24 21:04:03 +0530

    multiplication tables

---
+++ tables.java
@@ -0,0 +1,12 @@
+public class tables {
+    public static void main(String[] args) {
+        int n=10;
+        int i= 1;
+        int  table ;
+        while(i<=n){
+            table = n*i;
+            System.out.println(+n +" * "  +  i + " = " + table );
+            i++;
+        }
+    }
+}
